e60fb224147d7530655efeedaf89ee2ae282be84
Mounir Ybanez <rinuom91@gmail.com>
2016-04-18 16:28:48 +0800

    Added export variables for the editor.
    - Added `Run All Suites`, `Run Only Suite`, `Selected Suites`.
    - Added `Run All Test Cases`, `Run Only Test Case`, `Selected Test Cases`.

--- unit-test/test_base.gd
+++ unit-test/test_base.gd
@@ -1,47 +1,127 @@
+tool
 
 extends Node
 
-var test_methods = StringArray()
-var current_method = ""
+export var run_all_test_cases = true
+export var run_only_test_case = ""
+export var selected_test_cases = StringArray()
+
+var test_cases = Array()
+var current_test_case = ""
 var fail = 0
 var expectations = Dictionary()
+var error = 0
+var success = 0
 
-func run():
-	_get_test_methods()
-	_run_test_methods()
-	_evaluate_tests()
+func _ready():
+	_get_test_cases()
+	_sanitize_selected_test_cases()
 
-func _get_test_methods():
+func _get_test_cases():
 	for method in get_method_list():
-		var name = method["name"]
-		if name.begins_with("test_") and should_execute_test(name):
-			test_methods.push_back(name)
+		var test_case = method["name"]
+		if test_case.begins_with("test_"):
+			test_cases.push_back(test_case)
 
-func _run_test_methods():
-	for method in test_methods:
-		current_method = method
-		call(method)
+func _run_test_case(test_case):
+	call(test_case)
 
-func _evaluate_tests():
-	if not has_expectations():
+func _run_test_cases(tests):
+	for test_case in tests:
+		current_test_case = test_case
+		_run_test_case(test_case)
+
+func _evaluate():
+	if not has_expectations() and not _has_error():
 		if _is_succeeded():
 			print("[", get_name(), "] TEST SUCCESS")
 		else:
 			print("[", get_name(), "] TEST FAIL: ", fail)
+
+func _sanitize_selected_test_cases():
+	# Array of non-empty test cases
+	var array = Array()
+	
+	# Check for empty
+	for test_case in selected_test_cases:
+		if not test_case.empty():
+			array.push_back(test_case)
+	
+	# Contains non-redundant test cases
+	var array_2 = Array()
+	
+	# Check for duplicates
+	for test_case in array:
+		if array_2.find(test_case) < 0:
+			array_2.push_back(test_case)
+	
+	# Clear selected test cases
+	selected_test_cases.resize(0)
+	for test_case in array_2:
+		selected_test_cases.push_back(test_case)
+
+func _has_error():
+	return error > 0
 
 func _is_succeeded():
 	return fail == 0
 
 func _print_fail_message(message):
 	fail += 1
-	print("FAIL: ", current_method, " > ", message)
+	var fail_message = str("FAIL: ", current_test_case, " > ", message)
+	_print_message(fail_message)
+
+func _print_success_message(test_case):
+	success += 1
+	var success_message = str("OK: ", test_case)
+	_print_message(success_message)
+
+func _print_message(message):
+	print("[", get_name(), "] ", message)
+
+func _raise_error(message):
+	error += 1
+	_print_message(message)
 
 func _on_finish_expectation(expectation):
 	expectations.erase(expectation.get_name())
-	_evaluate_tests()
+	_evaluate()
 
-func should_execute_test(method):
-	return true
+func _has_test_case(test_case):
+	var ok = true
+	if test_cases.find(test_case) < 0:
+		ok = false
+		var message = ""
+		if not test_case.empty():
+			message = str("ERROR: ", test_case, " does not exist")
+		else:
+			message = str("ERROR: You provided an empty test case name.")
+		_raise_error(message)
+	return ok
+
+func _will_run_only_one_test_case():
+	var ok = false
+	if _has_test_case(run_only_test_case):
+		ok = true
+	return ok
+
+func _will_run_selected_test_cases():
+	var ok = false
+	for test_case in selected_test_cases:
+		if _has_test_case(test_case):
+			ok = true
+		else:
+			ok = false
+	return ok
+
+func run():
+	if run_all_test_cases:
+		_run_test_cases(test_cases)
+	elif _will_run_only_one_test_case():
+		_run_test_case(run_only_test_case)
+	elif _will_run_selected_test_cases():
+		_run_test_cases(selected_test_cases)
+	_evaluate()
 
 func add_expectation(expectation):
 	expectation.connect("on_finish", self, "_on_finish_expectation")
@@ -58,3 +138,4 @@ func assert_true(condition, message):
 func assert_false(condition, message):
 	if condition:
 		_print_fail_message(message)
+	
--- unit-test/unit_test.gd
+++ unit-test/unit_test.gd
@@ -2,21 +2,77 @@ tool
 
 extends Node
 
-export var run_all = true
-export var run_only = ""
-export var selected = []
+export var run_all_suites = true
+export var run_only_suite = ""
+export var selected_suites = StringArray()
+
+var suites = Array()
 
 func _ready():
-	if run_all or (run_only.empty() and selected.empty()):
-		for test in get_children():
-			test.run()
+	get_suites()
+	sanitize_selected_suites()
+	
+	if run_all_suites:
+		for suite in suites:
+			run_suite(suite)
+	elif will_run_only_one_suite():
+		run_suite(run_only_suite)
+	elif will_run_selected_suites():
+		for suite in selected_suites:
+			run_suite(suite)
 	else:
-		if not run_only.empty():
-			run_test(run_only)
-		else:
-			for test_name in selected:
-				run_test(test_name)
+		print("ERROR: 0 test suites executed.")
 
-func run_test(name):
-	var test = get_node(name)
-	test.run()
+func get_suites():
+	for suite in get_children():
+		var name = suite.get_name()
+		suites.push_back(name)
+
+func sanitize_selected_suites():
+	# Array of non-empty suite names
+	var array = Array()
+	
+	# Check for empty
+	for suite in selected_suites:
+		if not suite.empty():
+			array.push_back(suite)
+	
+	# Contains non-redundant suite names
+	var array_2 = Array()
+	
+	# Check for duplicates
+	for suite in array:
+		if array_2.find(suite) < 0:
+			array_2.push_back(suite)
+	
+	# Clear selected suites
+	selected_suites.resize(0)
+	for suite in array_2:
+		selected_suites.push_back(suite)
+
+func has_suite(name):
+	var ok = true
+	if suites.find(name) < 0:
+		ok = false
+		if not name.empty():
+			print("ERROR: ", name, " does not exist.")
+	return ok
+
+func will_run_selected_suites():
+	var ok = false
+	for suite in selected_suites:
+		if has_suite(suite):
+			ok = true
+		else:
+			ok = false
+	return ok
+
+func will_run_only_one_suite():
+	var ok = false
+	if has_suite(run_only_suite):
+		ok = true
+	return ok
+
+func run_suite(name):
+	var suite = get_node(name)
+	suite.run()
