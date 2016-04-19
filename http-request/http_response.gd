8aee575fc639e61833d70fa91097e4f569cbf3ae
Mounir Ybanez <rinuom91@gmail.com>
2016-04-19 16:33:00 +0800

    Refactored http response for setter and getter.

--- http-request/http_response.gd
+++ http-request/http_response.gd
@@ -1,11 +1,33 @@
 
 extends Reference
 
-var body
-var content_length = 0
-var status_code = 0
-var headers
-var error
+var body setget ,get_body
+var status_code setget ,get_status_code
+var header setget ,get_header
+var error setget set_error,get_error
 
-func _init():
-	pass
+func _init(body=RawArray(), status_code=0, header=[]):
+	self.body = body
+	self.status_code = status_code
+	self.header = header
+
+func get_body():
+	return body
+
+func get_status_code():
+	return status_code
+
+func get_header():
+	return header
+
+func has_error():
+	if error == null:
+		return false
+	else:
+		return true
+
+func set_error(what):
+	error = what
+
+func get_error():
+	return error
