8494e9b30c4c0adc5321f9b310822d087d3dbdbf
Mounir Ybanez <rinuom91@gmail.com>
2016-04-22 16:20:33 +0800

    Refactored test base.
    - Added signal when suite has finished.
    - Added count for overall assertion, failed assertion, and expecting test case.
    - Added setter for the  property.
    - Added `case` parameter when asserting.
    - Added `case` paremeter on failed assertion.
    - Handled the setting of case and suite of an expectation.
    - Added `info` property for summary.
    - Added status constants: `TEST_SUCCESS`, `TEST_FAIL`, `TEST_EXPECTING`.
    - Added method on printing expectation message.

--- unit-test/test_base.gd
+++ unit-test/test_base.gd
@@ -2,16 +2,27 @@ tool
 
 extends Node
 
+signal suite_on_finish(suite)
+
+const TEST_SUCCESS = 0
+const TEST_EXPECTING = 1
+const TEST_FAIL = 2
+
 export var run_all_test_cases = true
 export var run_only_test_case = ""
 export var selected_test_cases = StringArray()
 
+var expectations = Dictionary()
 var test_cases = Array()
 var current_test_case = ""
-var fail = 0
-var expectations = Dictionary()
 var error = 0
 var success = 0
+var fail = 0
+var expecting = 0
+var assertions = 0
+var assert_fail = 0
+
+var info = {}
 
 func _ready():
 	_get_test_cases()
@@ -25,7 +36,7 @@ func _get_test_cases():
 
 func _run_test_case(test_case):
 	setup()
-	current_test_case = test_case
+	set_current_test_case(test_case)
 	call(test_case)
 	teardown()
 
@@ -34,14 +45,21 @@ func _run_test_cases(tests):
 		_raise_error("0 test cases executed")
 	else:
 		for test_case in tests:
+			info[test_case] = TEST_SUCCESS
 			_run_test_case(test_case)
+			if info[test_case] == TEST_SUCCESS:
+				_on_success(test_case)
+			elif info[test_case] == TEST_EXPECTING:
+				_on_expecting(test_case)
 
 func _evaluate():
-	if not has_expectations() and not _has_error():
-		if _is_succeeded():
-			_print_message("TEST SUCCESS")
-		else:
-			_print_message(str("TEST FAIL: ", fail))
+	if not _has_expectations():
+		if not has_error():
+			if _is_succeeded():
+				_print_message("TEST SUCCESS")
+			else:
+				_print_message(str("TEST FAIL: ", fail))
+		emit_signal("suite_on_finish", self)
 
 func _sanitize_selected_test_cases():
 	# Array of non-empty test cases
@@ -65,21 +83,20 @@ func _sanitize_selected_test_cases():
 	for test_case in array_2:
 		selected_test_cases.push_back(test_case)
 
-func _has_error():
-	return error > 0
-
 func _is_succeeded():
 	return fail == 0
 
-func _print_fail_message(message):
-	fail += 1
-	var fail_message = str("FAIL: ", current_test_case, " > ", message)
+func _print_expecting_message(test_case):
+	var message = str("EXPECTING: ", test_case)
+	_print_message(message)
+
+func _print_fail_message(test_case, message):
+	var fail_message = str("FAIL: ", test_case, " > ", message)
 	_print_message(fail_message)
 
 func _print_success_message(test_case):
-	success += 1
-	var success_message = str("OK: ", test_case)
-	_print_message(success_message)
+	var message = str("OK: ", test_case)
+	_print_message(message)
 
 func _print_message(message):
 	print("[", get_name(), "] ", message)
@@ -90,6 +107,9 @@ func _raise_error(message):
 
 func _on_finish_expectation(expectation):
 	expectations.erase(expectation.get_name())
+	if info[expectation.get_case()] == TEST_EXPECTING:
+		info[expectation.get_case()] = TEST_SUCCESS
+		_on_success(expectation.get_case())
 	_evaluate()
 
 func _has_test_case(test_case):
@@ -119,6 +139,57 @@ func _will_run_selected_test_cases():
 			ok = false
 	return ok
 
+func _on_assert():
+	assertions += 1
+
+func _on_assert_fail(message, case=""):
+	assert_fail += 1
+	_on_fail(message, case)
+
+func _on_success(test_case):
+	success += 1
+	_print_success_message(test_case)
+
+func _on_expecting(test_case):
+	expecting += 1
+	_print_expecting_message(test_case)
+
+func _on_fail(message, case=""):
+	var test_case = case
+	if test_case.empty():
+		test_case = current_test_case
+	if info[test_case] != TEST_FAIL:
+		fail += 1
+	info[test_case] = TEST_FAIL
+	_print_fail_message(test_case, message)
+
+func _has_expectations():
+	return expectations.size() > 0
+
+func has_error():
+	return error > 0
+	
+func get_success_count():
+	return success
+
+func get_fail_count():
+	return fail
+
+func get_assertion_count():
+	return assertions
+
+func get_failed_assertion_count():
+	return assert_fail
+
+func get_test_case_count():
+	return test_cases.size()
+
+func get_expectation_count():
+	return expecting
+
+func set_current_test_case(case):
+	current_test_case = case
+
 func setup():
 	pass
 
@@ -135,49 +206,58 @@ func run():
 	_evaluate()
 
 func add_expectation(expectation):
+	info[current_test_case] = TEST_EXPECTING
+	expectation.set_suite(self)
+	expectation.set_case(current_test_case)
 	expectation.connect("on_finish", self, "_on_finish_expectation")
 	var name = expectation.get_name()
 	expectations[name] = expectation
 
-func has_expectations():
-	return expectations.size() > 0
+func fail(message, case=""):
+	_on_fail(message, case)
 
-func fail(message):
-	_print_fail_message(message)
-
-func assert_true(condition, message):
+func assert_true(condition, message, case=""):
+	_on_assert()
 	if not condition:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_false(condition, message):
+func assert_false(condition, message, case=""):
+	_on_assert()
 	if condition:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_null(variable, message):
+func assert_null(variable, message, case=""):
+	_on_assert()
 	if variable != null:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_not_null(variable, message):
+func assert_not_null(variable, message, case=""):
+	_on_assert()
 	if variable == null:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_empty(variable, message):
+func assert_empty(variable, message, case=""):
+	_on_assert()
 	if not variable.empty():
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_not_empty(variable, message):
+func assert_not_empty(variable, message, case=""):
+	_on_assert()
 	if variable.empty():
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_equal(var1, var2, message):
+func assert_equal(var1, var2, message, case=""):
+	_on_assert()
 	if var1 != var2:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_not_equal(var1, var2, message):
+func assert_not_equal(var1, var2, message, case=""):
+	_on_assert()
 	if var1 == var2:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_equal_array(arr1, arr2, message):
+func assert_equal_array(arr1, arr2, message, case=""):
+	_on_assert()
 	var ok = true
 	if arr1.size() == arr2.size():
 		for i in range(arr1.size()):
@@ -190,9 +270,10 @@ func assert_equal_array(arr1, arr2, message):
 		ok = false
 	
 	if not ok:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
 
-func assert_equal_dictionary(dict1, dict2, message):
+func assert_equal_dictionary(dict1, dict2, message, case=""):
+	_on_assert()
 	var ok = true
 	if (dict1.size() == dict2.size() and
 		dict1.keys().size() == dict2.keys().size()):
@@ -210,4 +291,4 @@ func assert_equal_dictionary(dict1, dict2, message):
 		ok = false
 	
 	if not ok:
-		_print_fail_message(message)
+		_on_assert_fail(message, case)
