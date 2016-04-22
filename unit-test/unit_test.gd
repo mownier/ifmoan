1bfd946e9975a4e638c0a340fb7b58804ac4aca9
Mounir Ybanez <rinuom91@gmail.com>
2016-04-22 16:29:43 +0800

    Added printing of summary.
    - Checked if should print summary whenever a test suite has finished.
    - This will print info if there are errors, failures, assertions, and overall info.

--- unit-test/unit_test.gd
+++ unit-test/unit_test.gd
@@ -7,6 +7,7 @@ export var run_only_suite = ""
 export var selected_suites = StringArray()
 
 var suites = Array()
+var completed = 0
 
 func _ready():
 	get_suites()
@@ -25,8 +26,51 @@ func _ready():
 
 func get_suites():
 	for suite in get_children():
+		suite.connect("suite_on_finish", self, "suite_on_finish")
 		var name = suite.get_name()
 		suites.push_back(name)
+
+func suite_on_finish(suite):
+	completed += 1
+	if should_print_summary():
+		print_summary()
+
+func should_print_summary():
+	return completed == suites.size()
+
+func print_summary():
+	var success = 0
+	var fail = 0
+	var asserts = 0
+	var cases = 0
+	var expectation = 0
+	var fail_assert = 0
+	var errors = 0
+	var suite_count = suites.size()
+	for name in suites:
+		var suite = get_node(name)
+		if suite.has_error():
+			errors += 1
+		success += suite.get_success_count()
+		fail += suite.get_fail_count()
+		asserts += suite.get_assertion_count()
+		cases += suite.get_test_case_count()
+		expectation += suite.get_expectation_count()
+		fail_assert += suite.get_failed_assertion_count()
+	
+	print("\r")
+	print("===================================")
+	print("SUMMARY: ")
+	if errors > 0:
+		print(">>> ERROR: ", errors, " test suites.")
+	print(suite_count, " test suites.")
+	print(cases, " test cases.")
+	print(success, " test cases succeded.")
+	print(fail, " test cases failed.")
+	print(asserts, " assertions performed.")
+	print(fail_assert, " assertions failed.")
+	print(expectation, " expectations finished.")
+	print("===================================")
 
 func sanitize_selected_suites():
 	# Array of non-empty suite names
