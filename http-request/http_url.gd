b99f5b800230b83a69645632ad4b6703cba143d7
Mounir Ybanez <rinuom91@gmail.com>
2016-04-26 17:16:14 +0800

    Changed url segment parsing.
    - Added individual methods for individual segment: scheme, user, host, port, path, query, fragment.
    - Removed `is_valid` method. It seems that the pattern has an error for some url string.

--- http-request/http_url.gd
+++ http-request/http_url.gd
@@ -6,54 +6,78 @@ var url_string
 func _init(url):
 	url_string = url
 
-func parse_segments():
-	var segments = {}
-	if is_valid():
-		var pattern = "([a-z][a-z0-9+\\-.]*):\/\/([a-z0-9\\-._~%!$&\'()*+,;=]+@)?([a-z0-9\\-._~%]+|\\[[a-f0-9:.]+\\]|\\[v[a-f0-9][a-z0-9\\-._~%!$&\'()*+,;=:]+\\])(:[0-9]+)?([a-z0-9\\-._~%!$&\'()*+,;=:@\/]*)(\\?[a-z0-9\\-._~%!$&\'()*+,;=:@\/?]*)?(\\#[a-z0-9\\-._~%!$&\'()*+,;=:@\/?]*)?"
-		var regex = RegEx.new()
-		regex.compile(pattern)
-		if regex.is_valid():
-			regex.find(url_string)
-			for i in range(regex.get_captures().size()):
-				if i > 0:
-					var segment = regex.get_capture(i)
-					if segment.empty():
-						continue
-					if i == 1:
-						segments["scheme"] = segment
-					elif i == 2:
-						if segment.length() > 1:
-							segments["user"] = segment.substr(0, segment.length() - 1)
-					elif i == 3:
-						segments["host"] = segment
-					elif i == 4:
-						if segment.length() > 1:
-							segments["port"] = int(segment.substr(1, segment.length() - 1))
-					elif i == 5:
-						if segment.empty():
-							segments["path"] = "/"
-						else:
-							segments["path"] = segment
-					elif i == 6:
-						if segment.length() > 1:
-							segments["query"] = segment.substr(1, segment.length() - 1)
-					elif i == 7:
-						if segment.length() > 1:
-							segments["fragment"] = segment.substr(1, segment.length() - 1)
-	return segments
-
-func is_valid():
-	var pattern = "([a-z][a-z0-9+\\-.]*:(\/\/([a-z0-9\\-._~%!$&\'()*+,;=]+@)?([a-z0-9\\-._~%]+|\\[[a-f0-9:.]+\\]|\\[v[a-f0-9][a-z0-9\\-._~%!$&\'()*+,;=:]+\\])(:[0-9]+)?(\/[a-z0-9\\-._~%!$&\'()*+,;=:@]+)*\/?|(\/?[a-z0-9\\-._~%!$&\'()*+,;=:@]+(\/[a-z0-9\\-._~%!$&\'()*+,;=:@]+)*\/?)?)|([a-z0-9\\-._~%!$&\'()*+,;=@]+(\/[a-z0-9\\-._~%!$&\'()*+,;=:@]+)*\/?|(\/[a-z0-9\\-._~%!$&\'()*+,;=:@]+)+\/?))(\\?[a-z0-9\\-._~%!$&\'()*+,;=:@\/?]*)?(\\#[a-z0-9\\-._~%!$&\'()*+,;=:@\/?]*)?"
+func _get_segment(pattern):
 	var regex = RegEx.new()
 	regex.compile(pattern)
 	regex.find(url_string)
-	if regex.get_capture_count() > 0 and regex.get_capture(0) == url_string:
-		return true
+	if regex.get_capture_count() > 0:
+		var last = regex.get_capture_count() - 1
+		return regex.get_capture(last)
 	else:
-		return false
+		return ""
+
+func parse_segments():
+	var segments = {}
+	
+	var scheme = get_scheme()
+	var user = get_user()
+	var host = get_host()
+	var port = get_port()
+	var path = get_path()
+	var query = get_query()
+	var fragment = get_fragment()
+	
+	if not scheme.empty():
+		segments["scheme"] = scheme
+	if not user.empty():
+		segments["user"] = user
+	if not host.empty():
+		segments["host"] = host
+	if not port.empty():
+		segments["port"] = int(port)
+	if not path.empty():
+		segments["path"] = path
+	if not query.empty():
+		segments["query"] = query
+	if not fragment.empty():
+		segments["fragment"] = fragment
+	
+	return segments
 
 func is_ssl_enabled(scheme):
 	if scheme == "https":
 		return true
 	else:
 		return false
+
+func get_scheme():
+	var pattern = "^([a-z][a-z0-9+\\-.]*):"
+	return _get_segment(pattern)
+
+func get_user():
+	var pattern = "^[a-z0-9+\\-.]+:\/\/([a-z0-9\\-._~%!$&\'()*+,;=]+)@"
+	return _get_segment(pattern)
+
+func get_host():
+	var pattern = "^[a-z][a-z0-9+\\-.]*:\/\/([a-z0-9\\-._~%!$&\'()*+,;=]+@)?([a-z0-9\\-._~%]+|\u21B5\r\n\\[[a-z0-9\\-._~%!$&\'()*+,;=:]+\\])"
+	return _get_segment(pattern)
+
+func get_port():
+	var pattern = "^[a-z][a-z0-9+\\-.]*:\/\/([a-z0-9\\-._~%!$&\'()*+,;=]+@)?([a-z0-9\\-._~%]+|\\[[a-z0-9\\-._~%!$&\'()*+,;=:]+\\]):([0-9]+)"
+	return _get_segment(pattern)
+
+func get_path():
+	var pattern = "([a-z][a-z0-9+\\-.]*:(\/\/[^\/?#]+)?)?([a-z0-9\\-._~%!$&\'()*+,;=:@\/]*)"
+	var path = _get_segment(pattern)
+	if not path.empty() and path.begins_with("/"):
+		return path
+	else:
+		return "/"
+
+func get_query():
+	var pattern = "^[^?#]+\\?([^#]+)"
+	return _get_segment(pattern)
+
+func get_fragment():
+	var pattern = "#(.+)"
+	return _get_segment(pattern)
